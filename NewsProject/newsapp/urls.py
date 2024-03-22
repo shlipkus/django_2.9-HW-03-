561e48befb21833dc79e7fc52b393aec1c820029
AlexShlipkus <Shlipkus@gmail.com>
2024-03-22 18:14:40 +0300

    D8.3

--- NewsProject/newsapp/urls.py
+++ NewsProject/newsapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', PostList.as_view(), name = 'post_list'),
-    path('news/', NewsList.as_view(), name = 'news_list'),
-    path('article/', ArticleList.as_view(), name = 'article_list'),
+    path('', cache_page(60)(PostList.as_view()), name = 'post_list'),
+    path('news/', cache_page(60*5)(NewsList.as_view()), name = 'news_list'),
+    path('article/', cache_page(60*5)(ArticleList.as_view()), name = 'article_list'),
     path('<int:pk>', PostDetails.as_view(), name = 'post_detail'),
     path('news/<int:pk>', PostDetails.as_view(), name = 'post_detail'),
     path('article/<int:pk>', PostDetails.as_view(), name = 'post_detail'),
